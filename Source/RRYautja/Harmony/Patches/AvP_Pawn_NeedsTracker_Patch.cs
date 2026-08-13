fafb4083c2cdfe8f06c127224e47a6f1d7655881
RhombusMaximus <RhombusMaximus@users.noreply.github.com>
2026-08-13 11:31:35 -0400

    fix: disable Pawn_NeedsTracker patch (NeedsTrackerTick renamed in 1.6)

--- Source/RRYautja/Harmony/Patches/AvP_Pawn_NeedsTracker_Patch.cs
+++ Source/RRYautja/Harmony/Patches/AvP_Pawn_NeedsTracker_Patch.cs
@@ -1,3 +1,4 @@
+#if false
 ﻿using RimWorld;
 using Verse;
 using HarmonyLib;
@@ -39,3 +40,4 @@ namespace RRYautja
         public static FieldInfo pawn = typeof(Pawn_NeedsTracker).GetField("pawn", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.GetField);
     }
 }
+#endif
